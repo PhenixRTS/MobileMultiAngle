a52f9ed8949f682a5e47ed5b86e81ccd5edc8348
Edijs Gorbunovs <edijs.gorbunovs@testdevlab.com>
2020-11-12 23:23:38 +0200

    Updated gradle libs and added missing copyrights
    * Updated gradle libs and added missing copyrights
    
    * Simplified view model extensions

--- Android/PhenixMultiAngle/app/src/main/java/com/phenixrts/suite/phenixmultiangle/common/ViewModelExtensions.kt
+++ Android/PhenixMultiAngle/app/src/main/java/com/phenixrts/suite/phenixmultiangle/common/ViewModelExtensions.kt
@@ -8,15 +8,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.phenixrts.suite.phenixmultiangle.MultiAngleApp
 
-inline fun <reified T : ViewModel> getViewModel(noinline owner: (() -> MultiAngleApp), noinline creator: (() -> T)? = null): T {
-    return if (creator == null)
-        ViewModelProvider(owner()).get(T::class.java)
-    else
-        ViewModelProvider(owner(), BaseViewModelFactory(creator)).get(T::class.java)
-}
-
 inline fun <reified T : ViewModel> lazyViewModel(noinline owner: (() -> MultiAngleApp), noinline creator: (() -> T)? = null) =
-    lazy { getViewModel(owner, creator) }
+    lazy {
+        if (creator == null)
+            ViewModelProvider(owner()).get(T::class.java)
+        else
+            ViewModelProvider(owner(), BaseViewModelFactory(creator)).get(T::class.java)
+    }
 
 class BaseViewModelFactory<T>(val creator: () -> T) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
